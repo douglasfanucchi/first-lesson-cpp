5e033766dda9adb164dbf0963473d5f6cc824871
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-09 04:04:34 -0300

    feat: implements Contact class

---
+++ ex01/Contact.cpp
@@ -0,0 +1,77 @@
+#include "Contact.hpp"
+
+std::string Contact::getFirstName() const
+{
+    return this->firstName;
+}
+
+std::string Contact::getLastName() const
+{
+    return this->lastName;
+}
+
+void Contact::setNickName(std::string nickName)
+{
+    this->nickName = nickName;
+}
+
+std::string Contact::getNickName() const
+{
+    return this->nickName;
+}
+
+void Contact::setIndex(int i)
+{
+    this->index = i;
+}
+
+int Contact::getIndex() const
+{
+    return this->index;
+}
+
+void Contact::setDarkestSecret(std::string darkestSecret)
+{
+    this->darkestSecret = darkestSecret;
+}
+
+std::string Contact::getDarkestSecret() const
+{
+    return this->darkestSecret;
+}
+
+void Contact::setPhoneNumber(std::string phoneNumber)
+{
+    this->phoneNumber = phoneNumber;
+}
+
+std::string Contact::getPhoneNumber() const
+{
+    return this->phoneNumber;
+}
+
+std::string Contact::getFormattedFirstName() const
+{
+    return this->formatString(this->getFirstName());
+}
+
+std::string Contact::getFormattedLastName() const
+{
+    return this->formatString(this->getLastName());
+}
+
+std::string Contact::getFormattedNickName() const
+{
+    return this->formatString(this->getNickName());
+}
+
+std::string Contact::formatString(std::string string) const
+{
+    if (string.size() < 11) {
+        int spacesToInsert = 10 - string.size();
+        string.insert(0, spacesToInsert, ' ');
+        return string;
+    }
+
+    return string.substr(0, 9) + ".";
+}
---
+++ ex01/Contact.hpp
@@ -0,0 +1,37 @@
+#ifndef CONTACT_H
+# define CONTACT_H
+
+#include <string>
+
+class Contact
+{
+    int index;
+    std::string const firstName;
+    std::string const lastName;
+    std::string nickName;
+    std::string darkestSecret;
+    std::string phoneNumber;
+
+    public:
+        Contact(
+            std::string firstName,
+            std::string lastName
+        ) : firstName(firstName), lastName(lastName) {}
+
+        int getIndex() const;
+        std::string getFirstName() const;
+        std::string getLastName() const;
+        std::string getNickName() const;
+        std::string getDarkestSecret() const;
+        std::string getPhoneNumber() const;
+        std::string getFormattedFirstName() const;
+        std::string getFormattedLastName() const;
+        std::string getFormattedNickName() const;
+        std::string formatString(std::string) const;
+        void setIndex(int index);
+        void setNickName(std::string);
+        void setDarkestSecret(std::string);
+        void setPhoneNumber(std::string);
+};
+
+#endif
