8941438f62a6d0bf307486c19b80fe4ebfc1155d
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-09 20:25:36 -0300

    tests: adds test that adds contact to the phonebook

--- ex01/tests/unit/PhoneBook.cpp
+++ ex01/tests/unit/PhoneBook.cpp
@@ -8,6 +8,25 @@ void test_should_demonstrates_phonebook_creation()
     ASSERT_EQ(0, phonebook.size());
 }
 
+void test_should_push_contact_to_the_back_of_phonebook()
+{
+    PhoneBook phonebook;
+    Contact contact, contact2;
+    const Contact *contacts = phonebook.getContacts();
+    contact.setPhoneNumber("11999999999");
+    contact2.setPhoneNumber("11888888888");
+    std::string first_expected = "11999999999";
+    std::string second_expected = "11888888888";
+
+    phonebook.push(contact);
+    phonebook.push(contact2);
+    std::string first_result = contacts[0].getPhoneNumber();
+    std::string second_result = contacts[1].getPhoneNumber();
+
+    ASSERT_STREQ(first_expected, first_result);
+    ASSERT_STREQ(second_expected, second_result);
+}
+
 void test_should_add_new_contact_and_chages_phonebooks_size()
 {
     PhoneBook phonebook;
@@ -24,4 +43,5 @@ void RUN_PHONEBOOK_SUITE()
 {
     test_should_demonstrates_phonebook_creation();
     test_should_add_new_contact_and_chages_phonebooks_size();
+    test_should_push_contact_to_the_back_of_phonebook();
 }
