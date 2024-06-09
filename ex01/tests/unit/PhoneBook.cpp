4ef3326871cddfa3b20c91fee9033a01c817485d
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-09 16:30:49 -0300

    tests: implements test to ensure PhoneBook::push(Contact) changesits size state

--- ex01/tests/unit/PhoneBook.cpp
+++ ex01/tests/unit/PhoneBook.cpp
@@ -8,7 +8,20 @@ void test_should_demonstrates_phonebook_creation()
     ASSERT_EQ(0, phonebook.size());
 }
 
+void test_should_add_new_contact_and_chages_phonebooks_size()
+{
+    PhoneBook phonebook;
+    Contact first, second, third;
+
+    phonebook.push(first);
+    phonebook.push(second);
+    phonebook.push(third);
+
+    ASSERT_EQ(3, phonebook.size());
+}
+
 void RUN_PHONEBOOK_SUITE()
 {
     test_should_demonstrates_phonebook_creation();
+    test_should_add_new_contact_and_chages_phonebooks_size();
 }
