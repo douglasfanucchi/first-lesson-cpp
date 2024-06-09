7da0fcfcb9bd7ad38db842fdcedf32570062301d
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-09 20:38:47 -0300

    tests: test pushes a new contact to phonebook when it\'s already fullfilled

--- ex01/tests/unit/PhoneBook.cpp
+++ ex01/tests/unit/PhoneBook.cpp
@@ -39,9 +39,37 @@ void test_should_add_new_contact_and_chages_phonebooks_size()
     ASSERT_EQ(3, phonebook.size());
 }
 
+void test_should_shift_first_contact_in_order_to_add_a_new_one_in_a_full_list_of_contacts()
+{
+    PhoneBook phonebook;
+    Contact first, second, third, fourth, fifth, sixth, seventh, eighth, nineth, result;
+    second.setFirstName("Second");
+    third.setFirstName("Third");
+    fourth.setFirstName("Fourth");
+    fifth.setFirstName("Fifth");
+    sixth.setFirstName("Sixth");
+    seventh.setFirstName("Seventh");
+    eighth.setFirstName("Eighth");
+    nineth.setFirstName("Nineth");
+    phonebook.push(first);
+    phonebook.push(second);
+    phonebook.push(third);
+    phonebook.push(fourth);
+    phonebook.push(fifth);
+    phonebook.push(sixth);
+    phonebook.push(seventh);
+    phonebook.push(eighth);
+
+    phonebook.push(nineth);
+
+    ASSERT_STREQ("Second", phonebook.getContacts()[0].getFirstName());
+    ASSERT_STREQ("Nineth", phonebook.getContacts()[7].getFirstName());
+}
+
 void RUN_PHONEBOOK_SUITE()
 {
     test_should_demonstrates_phonebook_creation();
     test_should_add_new_contact_and_chages_phonebooks_size();
     test_should_push_contact_to_the_back_of_phonebook();
+    test_should_shift_first_contact_in_order_to_add_a_new_one_in_a_full_list_of_contacts();
 }
