93dc1df75f22837ea8ef06e3055e129f8f738ffe
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-10 13:38:31 -0300

    tests: test adds a contact and list all contacts with SEARCH command. It also shows a field to input some user index

--- ex01/tests/e2e/Program.sh
+++ ex01/tests/e2e/Program.sh
@@ -42,6 +42,34 @@ function test_shows_input_fields_to_add_new_contact_when_passes_add_command()
     ASSERT_STREQ "$expected" "$result"
 }
 
+function test_display_all_contacts_when_type_SEARCH_command_and_also_display_new_field_requiring_user_index()
+{
+    local program_input=$'ADD\n'
+         program_input+=$'Douglas\n'
+         program_input+=$'Fanucchi\n'
+         program_input+=$'sementinha do mau\n'
+         program_input+=$'11999999999\n'
+         program_input+=$'deu em cima da ex do amigo\n'
+         program_input+=$'SEARCH'
+
+    local expected=$'First Name: \n'
+         expected+=$'Last Name: \n'
+         expected+=$'Nickname: \n'
+         expected+=$'Phonenumber: \n'
+         expected+=$'Darkest Secret: \n' 
+         expected+=$'---------------------------------------------\n'
+         expected+=$'|     INDEX|FIRST NAME| LAST NAME|  NICKNAME|\n'
+         expected+=$'---------------------------------------------\n'
+         expected+=$'|         0|   Douglas|  Fanucchi|sementinh.|\n'
+         expected+=$'---------------------------------------------\n'
+         expected+=$'User index to display: '
+
+    local result=$(echo "$program_input" | eval $executable)
+
+    ASSERT_STREQ "$expected" "$result"
+}
+
 test_exits_program_when_type_EXIT
 test_shows_input_fields_to_add_new_contact_when_passes_add_command
+test_display_all_contacts_when_type_SEARCH_command_and_also_display_new_field_requiring_user_index
 printf \\n
