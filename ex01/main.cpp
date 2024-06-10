8f1820aa90ac5f118694e6cedb4f452ca4d69088
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-10 20:29:46 -0300

    feat: instantiate program and runs it

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,3 +1,13 @@
+#include <Program.hpp>
+
 int main()
 {
+    Program program;
+    std::string command;
+
+    while (program.isRunning()) {
+        std::getline(std::cin, command);
+        program.run(command);
+    }
+    return 0;
 }
