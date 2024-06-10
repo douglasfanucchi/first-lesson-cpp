137fad301e2670307842a769cc5ae559c6175339
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-10 14:17:13 -0300

    tests: display what was expect and the result when fails assertion

--- ex01/tests/asserts.cpp
+++ ex01/tests/asserts.cpp
@@ -9,13 +9,28 @@ void ASSERT_TRUE(bool value) {
     }
 }
 
+void ERROR_MESSAGE(std::string expected, std::string result)
+{
+    std::cerr << std::endl << "EXPECTED: " << std::endl;
+    std::cerr << expected << std::endl;
+    std::cerr << "---------------------------------------";
+    std::cerr << std::endl << "RESULT: " << std::endl;
+    std::cerr << result << std::endl;
+}
+
 void ASSERT_STREQ(std::string expected, std::string result)
 {
     ASSERT_TRUE(expected == result);
+    if (expected != result) {
+        ERROR_MESSAGE(expected, result);
+    }
 }
 
 void ASSERT_EQ(int expected, int result)
 {
     ASSERT_TRUE(expected == result);
+    if (expected != result) {
+        ERROR_MESSAGE(std::to_string(expected), std::to_string(result));
+    }
 }
 #endif
