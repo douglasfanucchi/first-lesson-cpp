da773376d0371a6fff497ec656c02b21b7f5043e
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-10 14:54:48 -0300

    tests: test inputs an empty value into search index

--- ex01/tests/e2e/Program.sh
+++ ex01/tests/e2e/Program.sh
@@ -147,10 +147,40 @@ function test_should_receive_an_error_message_when_try_to_input_empty_value_into
     ASSERT_STREQ "$expected" "$result"
 }
 
+test_should_receive_a_not_found_message_when_inputs_empty_value_into_search_index()
+{
+    local program_input=$'ADD\n'
+         program_input+=$'Douglas\n'
+         program_input+=$'Fanucchi\n'
+         program_input+=$'sementinha do mau\n'
+         program_input+=$'1199999999\n'
+         program_input+=$'deu em cima da ex do amigo\n'
+         program_input+=$'SEARCH\n'
+         program_input+=$'\n'
+
+    local expected=$'First Name: \n'
+         expected+=$'Last Name: \n'
+         expected+=$'Nickname: \n'
+         expected+=$'Phonenumber: \n'
+         expected+=$'Darkest Secret: \n' 
+         expected+=$'---------------------------------------------\n'
+         expected+=$'|     INDEX|FIRST NAME| LAST NAME|  NICKNAME|\n'
+         expected+=$'---------------------------------------------\n'
+         expected+=$'|         0|   Douglas|  Fanucchi|sementinh.|\n'
+         expected+=$'---------------------------------------------\n'
+         expected+=$'User index to display: \n'
+         expected+=$'* CONTACT NOT FOUND *'
+
+    local result=$(echo "$program_input" | eval $executable)
+
+    ASSERT_STREQ "$expected" "$result"
+}
+
 test_exits_program_when_type_EXIT
 test_shows_input_fields_to_add_new_contact_when_passes_add_command
 test_displays_all_users_and_user_information_after_user_index_is_specified_on_SEARCH_command
 test_display_user_not_found_when_passes_invalid_index_on_SEARCH_command
 test_should_not_exit_program_when_exit_is_a_value_for_any_contact_field
 test_should_receive_an_error_message_when_try_to_input_empty_value_into_contact_field
+test_should_receive_a_not_found_message_when_inputs_empty_value_into_search_index
 printf \\n
