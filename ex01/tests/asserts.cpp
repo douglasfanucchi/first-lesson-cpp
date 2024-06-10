daa6db4b63807d148decb374ac12a2501810c495
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-10 14:12:21 -0300

    tests: makes string and integer assertions to use boolean assertion

--- ex01/tests/asserts.cpp
+++ ex01/tests/asserts.cpp
@@ -1,29 +1,21 @@
 #ifndef TESTS_H
 # define TESTS_H
 #include <iostream>
-void ASSERT_STREQ(std::string expected, std::string result)
-{
-    if (expected == result) {
-        std::cout << "✅";
-    } else {
-        std::cerr << "❌";
-    }
-}
-
-void ASSERT_EQ(int expected, int result)
-{
-    if (expected == result) {
-        std::cout << "✅";
-    } else {
-        std::cerr << "❌";
-    }
-}
-
 void ASSERT_TRUE(bool value) {
     if (value) {
         std::cout << "✅";
     } else {
         std::cerr << "❌";
     }
+}
+
+void ASSERT_STREQ(std::string expected, std::string result)
+{
+    ASSERT_TRUE(expected == result);
+}
+
+void ASSERT_EQ(int expected, int result)
+{
+    ASSERT_TRUE(expected == result);
 }
 #endif
