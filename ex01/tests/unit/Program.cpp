9710b5cc6b14e6c483a489712a672f7073339e2b
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-10 19:00:29 -0300

    tests: test calls Program::run(std::string) passing EXIT command

--- ex01/tests/unit/Program.cpp
+++ ex01/tests/unit/Program.cpp
@@ -8,7 +8,17 @@ void test_should_create_a_program_instance()
     ASSERT_TRUE(program.isRunning());
 }
 
+void test_should_call_run_method_passing_EXIT_command_and_expects_isRunning_to_change()
+{
+    Program program;
+
+    program.run("EXIT");
+
+    ASSERT_FALSE(program.isRunning());
+}
+
 void RUN_PROGRAM_SUITE()
 {
     test_should_create_a_program_instance();
+    test_should_call_run_method_passing_EXIT_command_and_expects_isRunning_to_change();
 }
