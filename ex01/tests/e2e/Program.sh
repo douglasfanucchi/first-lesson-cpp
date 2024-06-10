abc0c1e87a4536a48c0ac710aadbcce5d03accfd
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-10 12:13:10 -0300

    tests: test exits program with EXIT command

--- ex01/tests/e2e/Program.sh
+++ ex01/tests/e2e/Program.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-eval $1
+function ASSERT_STREQ()
+{
+if [[ $1 == $2 ]];
+then
+    printf ✅
+else
+    printf ❌
+fi
+}
+
+executable=$1
+
+function test_exits_program_when_type_EXIT()
+{
+local expected=""
+
+local result=$(echo -n "EXIT" | eval $executable)
+
+ASSERT_STREQ "$expected" "$result"
+}
+
+test_exits_program_when_type_EXIT
+printf \\n
