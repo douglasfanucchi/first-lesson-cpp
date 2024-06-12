8c825f2115ea2939cd1c66e1c2c7948b91beee6f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-12 18:20:29 -0300

    tests: test calls Utils::trim(std::string) passing a string starting with empty spaces

--- ex01/tests/unit/Utils.cpp
+++ ex01/tests/unit/Utils.cpp
@@ -1,5 +1,17 @@
+#include <Utils.hpp>
 #include <asserts.hpp>
+
+void test_should_call_trim_passing_a_string_with_empty_spaces_at_the_beggining_and_expects_the_string_without_it()
+{
+    std::string input("            my text");
+    std::string expected("my text");
+
+    std::string result = Utils::trim(input);
+
+    ASSERT_STREQ(expected, result);
+}
 
 void RUN_UTILS_SUITE()
 {
+    test_should_call_trim_passing_a_string_with_empty_spaces_at_the_beggining_and_expects_the_string_without_it();
 }
