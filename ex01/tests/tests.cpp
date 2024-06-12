33bc1b58a50c9d957b9578e2ca069d328521c0cd
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-06-12 18:09:08 -0300

    tests: adds unit tests file to Utils class

--- ex01/tests/tests.cpp
+++ ex01/tests/tests.cpp
@@ -3,11 +3,13 @@
 void RUN_CONTACT_SUITE();
 void RUN_PHONEBOOK_SUITE();
 void RUN_PROGRAM_SUITE();
+void RUN_UTILS_SUITE();
 
 int main()
 {
     RUN_CONTACT_SUITE();
     RUN_PHONEBOOK_SUITE();
     RUN_PROGRAM_SUITE();
+    RUN_UTILS_SUITE();
     std::cout << std::endl;
 }
---
+++ ex01/tests/unit/Utils.cpp
@@ -0,0 +1,5 @@
+#include <asserts.hpp>
+
+void RUN_UTILS_SUITE()
+{
+}
